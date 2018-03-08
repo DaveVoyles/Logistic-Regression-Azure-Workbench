1d4de14d52ed1d2fa4b942fef1812d2ac33ef2a5
Dave Voyles <dnvoyles@gmail.com>
2018-03-07 20:09:37 -0500

    init commit

--- linear_reg.py
+++ linear_reg.py
@@ -1,51 +1,97 @@
-# Linear regression on one-dimensional data using a closed form solution.
 # Please make sure matplotlib is included in the conda_dependencies.yml file.
-
-import numpy as np
+import pickle
+import sys
+import os
+import numpy             as np
+import pandas            as pd
 import matplotlib
 matplotlib.use('agg')
-import matplotlib.pyplot as plot
-fig = plot.figure()
+import matplotlib.pyplot as plt
 
-# load data
-X = []
-Y = []
-for line in open('data.csv'):
-    x, y = line.split(',')
-    X.append(float(x))
-    Y.append(float(y))
-    
-# turn them into numpy arrays so we can apply matrix operations
-X = np.array(X)
-Y = np.array(Y)
+# sklearn
+from sklearn.metrics         import confusion_matrix, mean_squared_error, explained_variance_score
+from sklearn.linear_model    import LinearRegression
+from sklearn.datasets        import load_boston
+from sklearn.model_selection import train_test_split
 
-# this is the common denominator
-denominator = X.dot(X) - X.mean() * X.sum()
+# Azure specific 
+from azureml.logging          import get_azureml_logger
+from azureml.dataprep.package import run
 
-# value of a
-a = (X.dot(Y) - Y.mean() * X.sum()) / denominator
+# Initialize the logger
+run_logger = get_azureml_logger() 
+# Create the outputs folder where the scatter plot & model.pkl will export to
+os.makedirs('./outputs', exist_ok=True)
+# Retrieve Boston data set
+boston = load_boston()
+# Boston data set is a series of keys
+# RETURNS: dict_keys(['feature_names', 'target', 'data', 'DESCR'])
+print('KEYS:')
+print(boston.keys())
+bos        = pd.DataFrame(boston.data)
+# Convert the index to the column names.
+bos.columns = boston.feature_names
+print('\nDATA SET: ')
+print(bos.head())
+# 'Target' key is actually the list of home prices
+# Create a column called 'PRICE' and add target prices to bos data frame
+bos['PRICE'] = boston.target
+# Remove target prices from our features, as this is what we will be trying to predict
+X = bos.drop('PRICE', axis=1)
 
-# value of b
-b = (Y.mean() * X.dot(X) - X.mean() * X.dot(Y)) / denominator
 
-# Yhat is simply aX + b
-Yhat = a * X + b
-print ("Coefficient: {0}, intercept: {1}".format(a, b))
+print('\n==========================================')
+print('Training 1')
+lr1 = LinearRegression()
+lr1.fit(X, bos.PRICE)
 
-# Plot the data and the fitted line, then save it into a png file in the output directory.
-ax = fig.gca()
-ax.scatter(X, Y)
-ax.plot(X, Yhat, color='magenta')
-fig.savefig('./outputs/lin.png')
+print('ESTIMATED INTERCEPT COEFFICIENT:',    round(lr1.intercept_, 2))
+print('NUMBER OF COEFFICIENTS:         ',    len(lr1.coef_          ))
 
-### compute r-squared ###
-# residual error of the prediction
-d1 = Y - Yhat
-# intrinsic error to mean
-d2 = Y - Y.mean()
+# Print first 5 results
+lr1.predict(X)[0:5]
+print('First 5 home values: ', boston.target [0:5])
+print('First 5 predictions: ', lr1.predict(X)[0:5])
 
-# if r2 is 1 (i.e., d1 is 0), this is a perfect model with no errors.
-# if r2 is 0 (i.e., d1 is the same as d2), this is a useless model as it is just the same as predicting mean.
-# if r2 is less than 0 (i.e., d1 is larger that d2), you are doing worse than predicting mean!!
-r2 = 1 - d1.dot(d1) / d2.dot(d2)
-print ("R-squared: {}.".format(r2))
+# serialize the model on disk in the special 'outputs' folder
+print('\n==========================================')
+print ("Export the model to model.pkl")
+f = open('./outputs/model.pkl', 'wb')
+pickle.dump(lr1, f)
+f.close()
+
+print('\n==========================================')
+print('Training 2: train_test_split')
+
+# load the model back from the 'outputs' folder into memory
+print("Import the model from model.pkl")
+f2 = open('./outputs/model.pkl', 'rb')
+lr2 = pickle.load(f2)
+
+X_train, X_test, y_train, y_test = train_test_split(X, bos.PRICE, test_size=0.33, random_state=5)
+lr2.fit(X_train, y_train)
+pred_train = lr2.predict(X_train)
+pred_test  = lr2.predict(X_test )
+mse_train  = round(mean_squared_error      (y_train, lr2.predict(X_train)), 4)
+mse_test   = round(mean_squared_error      (y_test,  lr2.predict(X_test )), 4)
+r_square   = round(explained_variance_score(y_test,  pred_test          ),  4)
+
+print('MSE w/ TRAIN data: ',  mse_train)
+print('MSE w/ TEST data:  ',  mse_test )
+print('R-Square:           ', r_square )
+
+# These results will appear in the Run Properties: Output in ML Workbench
+run_logger.log('MSE w/ TRAIN data:', mse_train)
+run_logger.log('MSE w/ TEST data: ', mse_test )
+run_logger.log('R-Square:         ', r_square )
+
+# Draw a plot. This will also appear in the Run Properties: Output in ML Workbench
+# Ideally, scatter plot should create a line. Model doesn't fit 100%, so scatter plot is not creating a line
+plt.scatter(y_test, pred_test                                 )
+plt.xlabel ('Prices: $Y_i$'                                   )
+plt.ylabel ('Predicted prices: $\hat{Y}_i$'                   )
+plt.title  ('Prices vs Predicted prices: $Y_i$ vs $\hat{Y}_i$')
+plt.savefig("./outputs/scatter.png", bbox_inches='tight'      )
+
+
+
---
+++ score.py
@@ -0,0 +1,78 @@
+# This script generates the scoring and schema files
+# Creates the schema, and holds the init and run functions needed to 
+# operationalize the Linear Regression sample
+
+# Import data collection library. Only supported for docker mode.
+# Functionality will be ignored when package isn't found
+try:
+    from azureml.datacollector import ModelDataCollector
+except ImportError:
+    print("Data collection is currently only supported in docker mode. May be disabled for local mode.")
+    # Mocking out model data collector functionality
+    class ModelDataCollector(object):
+        def nop(*args, **kw): pass
+        def __getattr__(self, _): return self.nop
+        def __init__(self, *args, **kw): return None
+    pass
+
+import os
+
+# Prepare the web service definition by authoring
+# init() and run() functions. Test the functions
+# before deploying the web service.
+def init():
+    global inputs_dc, prediction_dc
+    from sklearn.externals import joblib
+
+    # load the model from file into a global object
+    global model
+    model = joblib.load('model.pkl')
+
+    inputs_dc     = ModelDataCollector("model.pkl", identifier="inputs")
+    prediction_dc = ModelDataCollector("model.pkl", identifier="prediction")
+
+def run(input_df):
+    import json
+    
+    # append 40 random features just like the training script does it.
+    import numpy as np
+    n = 40
+    random_state = np.random.RandomState(0)
+    n_samples, n_features = input_df.shape
+    input_df = np.c_[input_df, random_state.randn(n_samples, n)]
+    inputs_dc.collect(input_df)
+
+    # make prediction using the model
+    pred = model.predict(input_df)
+    prediction_dc.collect(pred)
+    
+    # return all predictions
+    # return json.dumps(pred.tolist())
+    
+    # return just the first prediction
+    return json.dumps(str(pred[0]))
+
+def main():
+  from azureml.api.schema.dataTypes        import DataTypes
+  from azureml.api.schema.sampleDefinition import SampleDefinition
+  from azureml.api.realtime.services       import generate_schema
+  import pandas
+  
+  df = pandas.DataFrame(data=[[3.0, 3.6, 1.3, 0.25]], columns=['sepal length', 'sepal width','petal length','petal width'])
+
+  # Turn on data collection debug mode to view output in stdout
+  os.environ["AML_MODEL_DC_DEBUG"] = 'true'
+
+  # Test the output of the functions
+  init()
+  input1 = pandas.DataFrame([[3.0, 3.6, 1.3, 0.25]])
+  print("Result: " + run(input1))
+  
+  inputs = {"input_df": SampleDefinition(DataTypes.PANDAS, df)}
+  
+  #Genereate the schema
+  generate_schema(run_func=run, inputs=inputs, filepath='./outputs/service_schema.json')
+  print("Schema generated")
+
+if __name__ == "__main__":
+    main()
